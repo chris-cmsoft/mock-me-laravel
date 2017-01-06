f5e556670a3948151328df89a71da1598bd6a689
Chris Vermeulen <chris@cmsoft.co.za>
2017-01-06 13:26:15 +0200

    Accept any headers

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,7 +18,7 @@ class CorsMiddleware
         $response = $next($request);
         return $response
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Headers', '*');
     }
 }
